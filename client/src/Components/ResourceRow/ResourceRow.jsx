ec8dbe665269fe4bfbd826613fd935bccaa5560d
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-04-25 14:52:03 +0100

    resourcerow component added.

---
+++ client/src/Components/ResourceRow/ResourceRow.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+import { ReactComponent as FileIcon } from './file.svg'
+import { ReactComponent as FolderIcon } from './folder.svg'
+
+import './styles.css'
+
+export const ResourceRow = ({ resource, setState }) => {
+  return <tr>
+    <td onClick={() => {
+      setState(prevState => ({
+        ...prevState,
+        resources: resource.resources,
+        breadcums: [...prevState.breadcums, resource.name]
+      }))
+    }}>
+      <div className="resource">
+        {resource.type == "File" ? <FileIcon className="icon" /> : <FolderIcon className="icon" />}
+        <div>
+          {resource.name}
+        </div>
+      </div>
+    </td>
+  </tr>
+}
--- client/src/pages/Bucket/Bucket.jsx
+++ client/src/pages/Bucket/Bucket.jsx
@@ -1,38 +1,22 @@
-import React, { useContext } from 'react';
-import { withRouter, Link } from 'react-router-dom'
+import React, { useContext, useState } from 'react';
+import { withRouter } from 'react-router-dom'
+import { ResourceRow } from '../../Components/ResourceRow/ResourceRow'
 import { S3Context } from '../../context'
-import { ReactComponent as FileIcon } from './file.svg'
-import { ReactComponent as FolderIcon } from './folder.svg'
+
 import './styles.css'
-
-const Resource = ({ resource }) => {
-
-    const renderFile = () => <div className="resource">
-        <FileIcon className="icon" />
-        <div>
-            {resource.name}
-        </div>
-    </div>
-    const renderDir = () => <div className="resource">
-        <FolderIcon className="icon" />
-        <div>
-            {resource.name}
-        </div>
-    </div>
-
-    return <tr>
-        <td>
-            {resource.type == "File" ? renderFile() : renderDir()}
-        </td>
-    </tr>
-}
 
 
 const Component = ({ match }) => {
+
     const data = useContext(S3Context);
     const bucketName = match.params.bucketName;
     const bucketResources = data[bucketName].Resources;
-    console.log(bucketResources)
+    const initState = {
+        resources: bucketResources,
+        breadcums: [bucketName]
+    }
+    const [state, setState] = useState(initState)
+
     return <div className="buckets-table">
         <div className="table-head-container">
             <div className="table-text">
@@ -46,7 +30,11 @@ const Component = ({ match }) => {
                 </tr>
             </thead>
             <tbody>
-                {bucketResources.map((bucketData, index) => <Resource resource={bucketData} key={`${bucketData.name}-${index}`} />)}
+                {
+                    state.resources.map((bucketData, index) =>
+                        <ResourceRow resource={bucketData} key={`${bucketData.name}-${index}`} setState={setState} />
+                    )
+                }
             </tbody>
         </table>
     </div>
