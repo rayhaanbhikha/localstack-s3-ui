5bb7cc7451b6ae2d8c1deb04575575039d08766b
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-05-02 16:52:56 +0100

    resource file updated.

--- server/s3/resource.go
+++ server/s3/resource.go
@@ -7,16 +7,15 @@ import (
 
 type S3Resource struct {
 	// TODO: add metadata property.
-	Method      string `json:"-"`
-	BucketName  string `json:"bucketName"`
-	Name        string `json:"name"`
-	Type        string `json:"type"`
-	parentDirs  []string
-	Resources   []*S3Resource `json:"resources"`
-	ParentPath  string        `json:"-"`
-	Path        string        `json:"path"`
-	currentPath string
-	Data        string `json:"data"`
+	Method     string `json:"-"`
+	BucketName string `json:"bucketName"`
+	Name       string `json:"name"`
+	Type       string `json:"type"`
+	ParentDirs []string
+	Resources  []*S3Resource `json:"resources"`
+	ParentPath string        `json:"-"`
+	Path       string        `json:"path"`
+	Data       string        `json:"data"`
 }
 
 func (r *S3Resource) String() string {
@@ -24,10 +23,10 @@ func (r *S3Resource) String() string {
 		Bucket: %s
 		Name: %s
 		Type: %s
-		parentDirs: %v
+		ParentDirs: %v
 		Path: %s
-		CurrentPath: %s
-	`, r.BucketName, r.Name, r.Type, r.parentDirs, r.Path, r.currentPath)
+		ParentPath: %s
+	`, r.BucketName, r.Name, r.Type, r.ParentDirs, r.Path, r.ParentPath)
 }
 
 func newS3Resource(genRequest *GenRequest) *S3Resource {
@@ -39,24 +38,43 @@ func newS3Resource(genRequest *GenRequest) *S3Resource {
 
 	if len(path) == 1 {
 		return &S3Resource{
-			Method:      genRequest.Method,
-			BucketName:  path[0],
-			Type:        "Bucket",
-			Path:        genRequest.Path,
-			currentPath: genRequest.Path,
-			Data:        genRequest.Data,
+			Method:     genRequest.Method,
+			BucketName: path[0],
+			Type:       "Bucket",
+			Path:       genRequest.Path,
+			Data:       genRequest.Data,
 		}
 	}
 
 	return &S3Resource{
-		Method:      genRequest.Method,
-		BucketName:  path[0],
-		Name:        path[n-1],
-		Type:        "File",
-		ParentPath:  strings.Join(path[:n-1], "/"),
-		parentDirs:  path[1 : n-1],
-		Path:        genRequest.Path,
-		currentPath: "/" + path[0],
-		Data:        genRequest.Data,
+		Method:     genRequest.Method,
+		BucketName: path[0],
+		Name:       path[n-1],
+		Type:       "File",
+		ParentPath: strings.Join(path[:n-1], "/"),
+		ParentDirs: path[1 : n-1],
+		Path:       genRequest.Path,
+		Data:       genRequest.Data,
 	}
+}
+
+func EmptyDirResources(resource *S3Resource) []*S3Resource {
+	resources := make([]*S3Resource, 0)
+	n := len(resource.ParentDirs)
+	createResource := func(bucketName, name string, path []string) *S3Resource {
+		return &S3Resource{
+			Type:       "Directory",
+			Name:       name,
+			BucketName: bucketName,
+			Path:       strings.Join(path, "/"),
+			ParentPath: strings.Join(path[:len(path)-1], "/"),
+		}
+	}
+	currentPath := []string{resource.BucketName}
+	for _, parentDir := range resource.ParentDirs[:n-1] {
+		currentPath = append(currentPath, parentDir)
+		fmt.Println(createResource(resource.BucketName, parentDir, currentPath))
+	}
+
+	return resources
 }
