d0a1ca39595d6485082727fbc83eb012325eaa31
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-05-02 16:01:38 +0100

    insert statements refactored.

--- server/db/insert.go
+++ server/db/insert.go
@@ -8,10 +8,7 @@ import (
 
 // AddBucket creates a new bucket
 func (db *DB) AddBucket(bucketName string) (sql.Result, error) {
-	statement := `
-		INSERT INTO bucket(name) 
-		SELECT ? WHERE NOT EXISTS(SELECT bucket.name from bucket WHERE bucket.name=?);
-	`
+	statement := `REPLACE INTO bucket(name) VALUES(?) ON CONFLICT(name) DO NOTHING;`
 	stmt, err := db.Conn.Prepare(statement)
 	if err != nil {
 		return nil, err
@@ -22,13 +19,37 @@ func (db *DB) AddBucket(bucketName string) (sql.Result, error) {
 // AddResource creates a new resource
 func (db *DB) AddResource(s3Resource *s3.S3Resource) (sql.Result, error) {
 	statement := `
-	REPLACE INTO resource(path, parent_path, name, typeof, bucket_name, data) 
-	values(?,?,?,?,(SELECT bucket.name FROM bucket WHERE bucket.name=?),?) 
-	ON CONFLICT(path) DO UPDATE SET data=?
+	REPLACE INTO resource_file(path, name, data, bucket_name, parent_path) 
+	VALUES(?,?,?,
+		(SELECT bucket.name FROM bucket WHERE bucket.name=?)
+		(SELECT resource_dir.path FROM resource_dir WHERE resource_dir.path=?)
+	) ON CONFLICT(path) DO UPDATE SET data=?;
+`
+	stmt, err := db.Conn.Prepare(statement)
+	if err != nil {
+		return nil, err
+	}
+	return stmt.Exec(
+		s3Resource.Path,
+		s3Resource.Name,
+		s3Resource.Data,
+		s3Resource.BucketName,
+		s3Resource.ParentPath,
+		s3Resource.Data)
+}
+
+// AddFolderResource creates a new empty 'directory' resource
+func (db *DB) AddFolderResource(s3Resource *s3.S3Resource) (sql.Result, error) {
+	statement := `
+		REPLACE INTO resource_dir(path, name, bucket_name, parent_path) 
+		VALUES(?,?,
+			(SELECT bucket.name FROM bucket WHERE bucket.name=? ),
+			(SELECT resource_dir.path FROM resource_dir WHERE resource_dir.path=?)
+		)  ON CONFLICT(path) DO NOTHING;
 	`
 	stmt, err := db.Conn.Prepare(statement)
 	if err != nil {
 		return nil, err
 	}
-	return stmt.Exec(s3Resource.Path, s3Resource.ParentPath, s3Resource.Name, s3Resource.Type, s3Resource.BucketName, s3Resource.Data, s3Resource.Data)
+	return stmt.Exec(s3Resource.Path, s3Resource.Name, s3Resource.BucketName, s3Resource.ParentPath)
 }
