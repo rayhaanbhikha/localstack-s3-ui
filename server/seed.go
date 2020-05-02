43400c4d2cf45edd741b9380db1f5ffdd6e0b3c0
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-05-02 16:53:21 +0100

    seed script updated.

--- server/seed.go
+++ server/seed.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"log"
-
 	"github.com/rayhaanbhikha/localstack-s3-ui/db"
 	"github.com/rayhaanbhikha/localstack-s3-ui/s3"
 )
@@ -18,34 +15,38 @@ func seed(db *db.DB) error {
 
 	for _, s3Resource := range s3Resources {
 
-		if s3Resource.Type == "Bucket" {
-			switch s3Resource.Method {
-			case "PUT":
-				_, err := db.AddBucket(s3Resource.BucketName)
-				if err != nil {
-					log.Fatal(err)
-				}
-			case "DELETE":
-				_, err := db.DeleteBucket(s3Resource.BucketName)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-		} else {
-			fmt.Println(s3Resource)
-			switch s3Resource.Method {
-			case "PUT":
-				_, err := db.AddResource(s3Resource)
-				if err != nil {
-					log.Fatal(err)
-				}
-			case "DELETE":
-				_, err := db.DeleteResource(s3Resource)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
+		if s3Resource.Name == "index.html" && len(s3Resource.ParentDirs) == 3 {
+			s3.EmptyDirResources(s3Resource)
 		}
+
+		// if s3Resource.Type == "Bucket" {
+		// 	switch s3Resource.Method {
+		// 	case "PUT":
+		// 		_, err := db.AddBucket(s3Resource.BucketName)
+		// 		if err != nil {
+		// 			log.Fatal(err)
+		// 		}
+		// 	case "DELETE":
+		// 		_, err := db.DeleteBucket(s3Resource.BucketName)
+		// 		if err != nil {
+		// 			log.Fatal(err)
+		// 		}
+		// 	}
+		// } else {
+		// 	fmt.Println(s3Resource)
+		// 	switch s3Resource.Method {
+		// 	case "PUT":
+		// 		_, err := db.AddResource(s3Resource)
+		// 		if err != nil {
+		// 			log.Fatal(err)
+		// 		}
+		// 	case "DELETE":
+		// 		_, err := db.DeleteResource(s3Resource)
+		// 		if err != nil {
+		// 			log.Fatal(err)
+		// 		}
+		// 	}
+		// }
 
 		// fmt.Println(s3Resource)
 	}
