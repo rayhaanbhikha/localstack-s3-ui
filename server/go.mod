599d2b2fd252dc9ffe3ddea1bac611a97ed91827
Rayhaan Bhikha <rayhaan.bhikha@dazn.com>
2020-05-08 12:20:30 +0100

    server extracted into separate file.

--- server/go.mod
+++ server/go.mod
@@ -5,5 +5,5 @@ go 1.14
 require (
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gorilla/mux v1.7.4
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f
+	golang.org/x/sys v0.0.0-20200323222414-85ca7c5b95cd // indirect
 )
--- server/main.go
+++ server/main.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"log"
-	"net/http"
-	"os"
-	"os/signal"
-	"time"
 
-	mux "github.com/gorilla/mux"
 	"github.com/rayhaanbhikha/localstack-s3-ui/s3"
-	"golang.org/x/net/context"
 )
 
 func main() {
@@ -28,49 +22,4 @@ func main() {
 	}
 	defer watcher.Close()
 	startServer(rootNode)
-}
-
-func startServer(rootNode *s3.Node) {
-
-	r := mux.NewRouter()
-	r.PathPrefix("/api/resource/").
-		Handler(http.StripPrefix("/api/resource", resourcesHandler(rootNode))).
-		Methods("GET")
-
-	r.PathPrefix("/").
-		Handler(resourceHandler(rootNode)).
-		Methods("GET")
-
-	server := &http.Server{
-		Addr:    "127.0.0.1:8080",
-		Handler: r,
-	}
-
-	server.RegisterOnShutdown(func() {
-		log.Println("Shutting down server")
-	})
-
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-
-	go func() {
-		log.Println("Server starting on PORT 8080")
-
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Server failed to start: %s", err.Error())
-		}
-	}()
-
-	sig := <-c
-	log.Printf("Signal received: %s\n", sig.String())
-
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer func() {
-		cancel()
-	}()
-	err := server.Shutdown(ctx)
-
-	if err != nil && err != http.ErrServerClosed {
-		log.Fatalf("Err shutting down server: %s", err.Error())
-	}
 }
